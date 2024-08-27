d561be7bc40161b932d817f5a62896883af0ea08
Orion Lawlor <lawlor@alaska.edu>
2024-08-27 13:28:50 -0800

    Added the WASD player motion script from today's lecture.

---
+++ demo/scripts/player_move_WASD.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+var angle = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var turn = 0  # how much we want to turn
+	if (Input.is_physical_key_pressed(KEY_A)):
+		turn = -1
+	if (Input.is_physical_key_pressed(KEY_D)):
+		turn = +1
+	
+	var turnrate : float = 120.0 # in degrees/second
+	angle += turnrate * turn * delta
+	transform.basis = Basis(Vector3(0,1,0),deg_to_rad(angle))
+	
+	
+	var move = 0 # how far we want to move
+	if (Input.is_physical_key_pressed(KEY_W)):
+		move = +1
+	if (Input.is_physical_key_pressed(KEY_S)):
+		move = -1
+	
+	# The direction we want to move is our -Z axis
+	var movedir : Vector3 = -transform.basis.z
+	var moverate : float = 5.0 # in meters/second
+	transform.origin += delta * move * moverate * movedir
+
+	# For 2D rotations, it's not too bad to do the same thing with raw trig,
+	#  but for 3D using vectors is much better.
+	#if (Input.is_physical_key_pressed(KEY_W)):
+	#	transform.origin.x -= sin(deg_to_rad(angle)) * delta
+	#	transform.origin.z -= cos(deg_to_rad(angle)) * delta
