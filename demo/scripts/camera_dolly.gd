0b218a1bbf7bd5c0a62ca1cacf39c623fc2c0690
Orion Lawlor <lawlor@alaska.edu>
2024-09-26 17:17:24 -0800

    Fix direction on 'rate', so higher rate converges faster.

--- demo/scripts/camera_dolly.gd
+++ demo/scripts/camera_dolly.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 @export var track : Node3D   # We should track this object
 @export var offset : Vector3 = Vector3(0,2,4) # distance to camera
-@export var rate : float = 0.5 # rate of convergence to offset (higher = faster, units 1/seconds)
+@export var rate : float = 1.5 # rate of convergence to offset (higher = faster, units 1/seconds)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,6 +13,6 @@ func _process(delta: float) -> void:
 	var tbasis : Basis = track.global_transform.basis
 	
 	var target : Vector3 = track.global_position + tbasis*offset
-	global_position = lerp(global_position,target,delta/rate)
+	global_position = lerp(global_position,target,2.0*delta*rate)
 	
-	global_transform.basis = lerp (global_transform.basis,tbasis,delta/rate)
+	global_transform.basis = lerp (global_transform.basis,tbasis,delta*rate).orthonormalized()
